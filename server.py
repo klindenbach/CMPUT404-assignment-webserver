39b2bfe4bc9dadc81042c146068896eb66839639
Konrad Lindenbach <konrad.lindenbach@gmail.com>
2015-01-10 14:31:24 -0700

    Added Konrad and Ben to Copyright notices

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import SocketServer
 # coding: utf-8
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
+#           2014 Konrad Lindenbach, Ben Dubois
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
